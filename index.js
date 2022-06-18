b43747d8db36441976406f4856ec7d20d678dc7e
Biplob123121 <biplobkb123@gmail.com>
2022-06-18 09:22:19 +0600

    initialize origin cloud server

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//midleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a1gcb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('Hello From Origin Cloud Technologies!')
+});
+
+app.listen(port, () => {
+    console.log(`Origin Cloud app listening on port ${port}`)
+});
